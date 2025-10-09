160a69deaa0f5cb01b98275ac5d5a89ff3eb1456
Naruto <2045933252@qq.com>
2025-10-10 00:13:44 +0800

    提交--最终版--1

--- reserveBankend/src/main/java/com/naruto/reservebankend/ReserveBankendApplication.java
+++ reserveBankend/src/main/java/com/naruto/reservebankend/ReserveBankendApplication.java
@@ -3,11 +3,16 @@ package com.naruto.reservebankend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootApplication
 public class ReserveBankendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReserveBankendApplication.class, args);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.err.println(dateFormat.format(new Date()) + "   - 预约蒸镀服务启动成功...");
     }
 
 }
